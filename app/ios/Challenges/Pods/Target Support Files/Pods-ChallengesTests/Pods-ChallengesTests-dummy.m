b0bd4e478dc9a071c5decd5da582f808d6eba1b3
Paulo Renan <p.rsrizardi@gmail.com>
2018-04-06 14:21:40 -0400

    instalacao Pods Firebase

---
+++ app/ios/Challenges/Pods/Target Support Files/Pods-ChallengesTests/Pods-ChallengesTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChallengesTests : NSObject
+@end
+@implementation PodsDummy_Pods_ChallengesTests
+@end
---
+++ app/ios/Challenges/Pods/Target Support Files/Pods-ChallengesUITests/Pods-ChallengesUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChallengesUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ChallengesUITests
+@end
