b0ad01222070514e91da68c8d2d981e19d19e31d
Wender Galan <wendergalan2014@hotmail.com>
2018-04-24 18:05:26 -0400

    UPDATE, atualizando a activity de detalhes dos desafios e já preparando a activity para receber dados, assim como a validacão dos campos da activity de inserção de desafios...

--- app/android/Challenges/app/src/main/java/challenges/com/challenges/activities/CadastrarDesafioActivity.java
+++ app/android/Challenges/app/src/main/java/challenges/com/challenges/activities/CadastrarDesafioActivity.java
@@ -2,15 +2,25 @@ package challenges.com.challenges.activities;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import challenges.com.challenges.R;
+import challenges.com.challenges.adapter.HintAdapter;
+import challenges.com.challenges.model.Desafio;
+import challenges.com.challenges.util.Validator;
 
 public class CadastrarDesafioActivity extends AppCompatActivity {
 
@@ -20,7 +30,10 @@ public class CadastrarDesafioActivity extends AppCompatActivity {
     private EditText pontos;
     private EditText observacoes;
     private Button salvar;
-
+    private Spinner spinnerHabilidade;
+    private List<String> listaHabilidade;
+    private boolean habilidadeSelecionada = false;
+    private Desafio desafio;
 
 
     @SuppressLint("ResourceAsColor")
@@ -29,6 +42,7 @@ public class CadastrarDesafioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_desafio);
 
+        spinnerHabilidade = findViewById(R.id.spinnerHabilidades);
         titulo = findViewById(R.id.editTextTituloDesafio);
         recompensa = findViewById(R.id.editTextRecompensaDesafio);
         pontos = findViewById(R.id.editTextPontosDesafio);
@@ -43,27 +57,96 @@ public class CadastrarDesafioActivity extends AppCompatActivity {
                 finish();
             }
         });
+        desafio = new Desafio();
 
         //botao salvar
         salvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (validarCampos()){
+                    //seta o titulo
+                    desafio.setTitulo(titulo.getText().toString());
+                    //seta a recompensa se for diferente de vazio
+                    if (!recompensa.getText().toString().equals("")){
+                        desafio.setRecompensa(recompensa.getText().toString());
+                    }
+                    desafio.setPontos(Integer.parseInt(pontos.getText().toString()));
+                    if (!observacoes.getText().toString().equals("")){
+                        desafio.setObservacoes(observacoes.getText().toString());
+                    }
+
+                    /**
+                     * AGORA É SO CHAMAR O CONSTRUIR HASH E SALVAR O DESAFIO NO BANCO DE DADOS
+                     * */
+                    desafio.construirHash();
 
                 }
             }
         });
 
+        listaHabilidade = new ArrayList<String>();
+        listaHabilidade.add("Física");
+        listaHabilidade.add("Intelectual");
+        listaHabilidade.add("Criatividade");
+        listaHabilidade.add("Social");
+        listaHabilidade.add("Habilidade");
+        //seta o adapter para consumir toda a lista menos o último item porque o mesmo é o hint da caixa de seleção
+        HintAdapter adapterCategoria = new HintAdapter(this, R.layout.spinner_item, listaHabilidade);
+        adapterCategoria.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerHabilidade.setAdapter(adapterCategoria);
+        spinnerHabilidade.setSelection(adapterCategoria.getCount());
 
+        spinnerHabilidade.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String resultado = listaHabilidade.get(i).toString();
+                if (resultado.equals("Habilidade")){
+                    habilidadeSelecionada = true;
+                }else if (resultado.equals("Física")){
+                    desafio.setHabilidade("Física");
+                    habilidadeSelecionada = false;
+                }else if (resultado.equals("Intelectual")){
+                    desafio.setHabilidade("Intelectual");
+                    habilidadeSelecionada = false;
+                }else if (resultado.equals("Criatividade")){
+                    desafio.setHabilidade("Criatividade");
+                    habilidadeSelecionada = false;
+                }else if (resultado.equals("Social")){
+                    desafio.setHabilidade("Social");
+                    habilidadeSelecionada = false;
+                }
 
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
+            }
+        });
     }
 
     private boolean validarCampos() {
-
-
-
-        return true;
+        Validator validator = new Validator();
+        boolean retorno = true;
+        if (!validator.validateNotNull(titulo)) {
+            titulo.setError("Título vazio");
+            titulo.setFocusable(true);
+            titulo.requestFocus();
+            retorno = false;
+        }
+        if (!validator.validateNotNull(pontos)) {
+            pontos.setError("Pontos vazio");
+            pontos.setFocusable(true);
+            pontos.requestFocus();
+            retorno = false;
+        }
+        if (habilidadeSelecionada) {
+            TextView errorText3 = (TextView) spinnerHabilidade.getSelectedView();
+            errorText3.setError("Habilidade não selecionada");
+            errorText3.setTextColor(Color.RED);//just to highlight that this is an error
+            errorText3.setText("Habilidade não selecionada");//changes the selected item text to this
+            retorno = false;
+        }
+        return retorno;
     }
 
 
--- app/android/Challenges/app/src/main/java/challenges/com/challenges/activities/DesafioDetalhesActivity.java
+++ app/android/Challenges/app/src/main/java/challenges/com/challenges/activities/DesafioDetalhesActivity.java
@@ -1,15 +1,18 @@
 package challenges.com.challenges.activities;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 import challenges.com.challenges.R;
+import challenges.com.challenges.model.Desafio;
 
 public class DesafioDetalhesActivity extends AppCompatActivity {
 
@@ -24,6 +27,17 @@ public class DesafioDetalhesActivity extends AppCompatActivity {
     private TextView totalNaoFeito;
     private TextView totalRecompensas;
     private ImageView imagemHabilidade;
+    private Button primeiroBotao;
+    private Button segundoBotao;
+    private Desafio desafio;
+    /**TIPO vai ter três para poder saber qual activity será aberta
+     * responsavel que é a padrão,
+     * crianca,
+     * ajuda,
+     * completo(completar Desafio)
+     * aceitar (aceitar desafio)
+     * **/
+    private String tipo;
 
 
     @Override
@@ -50,6 +64,116 @@ public class DesafioDetalhesActivity extends AppCompatActivity {
         totalNaoFeito = findViewById(R.id.textViewNaoFeitoDetalhesDesafio);
         totalRecompensas = findViewById(R.id.textViewRecompensasDetalhesDesafio);
         imagemHabilidade = findViewById(R.id.imageViewHabilidadeDetalhesDesafio);
+        primeiroBotao = findViewById(R.id.buttonPrimeiro);
+        segundoBotao = findViewById(R.id.buttonSegundo);
+
+        //recuperar as intents
+        desafio = (Desafio) getIntent().getSerializableExtra("desafio");
+        tipo = getIntent().getStringExtra("tipo");
+
+        //seta os textview na acitivity
+        if (desafio != null){
+            if (desafio.getTitulo() != null){
+                titulo.setText(desafio.getTitulo());
+            }
+            if (desafio.getRecompensa() != null){
+                recompensa.setText("Recompensa: " + desafio.getRecompensa());
+            }
+            if (desafio.getHabilidade() != null){
+                habilidade.setText(desafio.getHabilidade());
+            }
+            if (desafio.getPontos() != 0){
+                pontos.setText("Pontos: " + desafio.getPontos());
+            }
+            /**TEM QUE SETAR A IMAGEM TAMBEM**/
+            /**SETAR A FREQUENCIA e BUSCAR O PROGRESSO E SETAR AQUI**/
+
+        }
+
+        if (tipo.equals("responsavel")){
+            primeiroBotao.setText("Editar");
+            primeiroBotao.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.fundo_botao_laranja_grande));
+            primeiroBotao.setVisibility(View.VISIBLE);
+            segundoBotao.setText("Excluir");
+            segundoBotao.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.fundo_botao_vermelho_grande));
+            segundoBotao.setVisibility(View.VISIBLE);
+        }else if (tipo.equals("crianca")){
+            primeiroBotao.setText("Completar Desafio");
+            primeiroBotao.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.fundo_botao_verde_grande));
+            primeiroBotao.setVisibility(View.VISIBLE);
+            segundoBotao.setText("Pedir ajuda");
+            segundoBotao.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.fundo_botao_azul_grande));
+            segundoBotao.setVisibility(View.VISIBLE);
+        }else if(tipo.equals("ajuda")){
+            primeiroBotao.setText("Aceitar pedido de ajuda");
+            primeiroBotao.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.fundo_botao_verde_grande));
+            primeiroBotao.setVisibility(View.VISIBLE);
+            segundoBotao.setText("Recusar pedido de ajuda");
+            segundoBotao.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.fundo_botao_vermelho_grande));
+            segundoBotao.setVisibility(View.VISIBLE);
+        }else if (tipo.equals("completo")){
+            primeiroBotao.setText("Completar desafio");
+            primeiroBotao.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.fundo_botao_verde_grande));
+            segundoBotao.setVisibility(View.INVISIBLE);
+        }else if (tipo.equals("aceitar")){
+            primeiroBotao.setText("Aceitar desafio");
+            primeiroBotao.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.fundo_botao_azul_grande));
+            segundoBotao.setVisibility(View.INVISIBLE);
+        }
+
+        primeiroBotao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (tipo.equals("responsavel")){
+
+
+                }else if (tipo.equals("crianca")){
+
+
+                }else if(tipo.equals("ajuda")){
+
+
+                }else if (tipo.equals("completo")){
+
+
+                }else if (tipo.equals("aceitar")){
+
+
+                }
+            }
+        });
+
+        segundoBotao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (tipo.equals("responsavel")){
+
+
+                }else if (tipo.equals("crianca")){
+
+
+                }else if(tipo.equals("ajuda")){
+
+
+                }else if (tipo.equals("completo")){
+
+
+                }else if (tipo.equals("aceitar")){
+
+
+                }
+            }
+        });
+
+
+        /**
+         * PRIMEIRO VEM POR INTENT O OBJETO DESAFIO PARA PREENCHER OS CAMPOS
+         *
+         * DEPOIS PEGA O INTENT TIPO E CHAMA OS BOTOES CERTOS
+         *
+         * **/
+
+
 
     }
 }
