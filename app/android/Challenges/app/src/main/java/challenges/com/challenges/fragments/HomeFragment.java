3020ddbbc64e65e006cbde56723b1dfed948c394
Matheus Pimentel <matheus200713@gmail.com>
2018-05-03 23:00:22 -0400

    atualizando layouts para constraint e arrumando fragment home

--- app/android/Challenges/app/src/main/java/challenges/com/challenges/fragments/HomeFragment.java
+++ app/android/Challenges/app/src/main/java/challenges/com/challenges/fragments/HomeFragment.java
@@ -19,42 +19,29 @@ import challenges.com.challenges.R;
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
-    private Button sair;
-    private Button detalheDesafio;
-
 
     public HomeFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home2, container, false);
-        sair = view.findViewById(R.id.button2);
-        detalheDesafio = view.findViewById(R.id.button5);
 
-        sair.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //desloga o usuario e volta para a tela incial de Login
-                FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
-                autenticacao.signOut();
-                Intent intent = new Intent(getActivity(), LoginActivity.class);
-                startActivity(intent);
-                getActivity().finish();
-            }
-        });
-
-//        detalheDesafio.setOnClickListener(new View.OnClickListener() {
+//        sair.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
-//                Intent intent = new Intent(getActivity(), DetalheDesafioActivity.class);
+//                //desloga o usuario e volta para a tela incial de Login
+//                FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+//                autenticacao.signOut();
+//                Intent intent = new Intent(getActivity(), LoginActivity.class);
 //                startActivity(intent);
+//                getActivity().finish();
 //            }
 //        });
+
         return view;
     }
 
