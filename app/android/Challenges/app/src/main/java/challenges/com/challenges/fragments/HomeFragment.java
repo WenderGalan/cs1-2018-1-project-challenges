6d75f5a8bc7216e314cc80efdb72bb127ca1f0d1
Matheus Pimentel <matheus200713@gmail.com>
2018-05-04 19:09:28 -0400

    implementação da logica para listagem de desafios, arrumando erros de layout

--- app/android/Challenges/app/src/main/java/challenges/com/challenges/fragments/HomeFragment.java
+++ app/android/Challenges/app/src/main/java/challenges/com/challenges/fragments/HomeFragment.java
@@ -1,24 +1,46 @@
 package challenges.com.challenges.fragments;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import com.google.firebase.auth.FirebaseAuth;
+import android.widget.Toast;
 
-import challenges.com.challenges.activities.LoginActivity;
-import challenges.com.challenges.config.ConfiguracaoFirebase;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import challenges.com.challenges.R;
+import challenges.com.challenges.activities.HomeResponsavelActivity;
+import challenges.com.challenges.adapter.DesafioAdapter;
+import challenges.com.challenges.config.ConfiguracaoFirebase;
+import challenges.com.challenges.model.Crianca;
+import challenges.com.challenges.model.Desafio;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+
+    private RecyclerView recyclerViewDesafioCrianca;
+    private RecyclerView recyclerViewSugestoesDesafioCrianca;
+
+    private FirebaseAuth autenticacao;
+    private Crianca crianca;
+    private DesafioAdapter desafioAdapter;
+
+    private String UID;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -29,6 +51,30 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home2, container, false);
+
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        String idUsuarioAtual = autenticacao.getCurrentUser().getUid();
+
+
+        recyclerViewDesafioCrianca = view.findViewById(R.id.recyclerViewDesafios);
+        recyclerViewSugestoesDesafioCrianca = view.findViewById(R.id.recyclerViewSugestoesDesafios);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), 2);
+        recyclerViewDesafioCrianca.setLayoutManager(layoutManager);
+//        recyclerViewSugestoesDesafioCrianca.setLayoutManager(layoutManager);
+
+        Toast.makeText(getContext(),idUsuarioAtual, Toast.LENGTH_LONG).show();
+
+        Query query = ConfiguracaoFirebase.getFirestore().collection("Desafios").whereEqualTo("crianca", idUsuarioAtual);
+        query.addSnapshotListener(getActivity(), new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+                List<Desafio> desafiosResult = documentSnapshots.toObjects(Desafio.class);
+                desafioAdapter = new DesafioAdapter((ArrayList<Desafio>) desafiosResult, "crianca");
+                recyclerViewDesafioCrianca.setAdapter(desafioAdapter);
+            }
+        });
 
 //        sair.setOnClickListener(new View.OnClickListener() {
 //            @Override
