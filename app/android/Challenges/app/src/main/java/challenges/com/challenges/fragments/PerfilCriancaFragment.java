57bb3fe3fc3b07ddbc0183769727717fdb1a9198
Wender Galan <wendergalan2014@hotmail.com>
2018-05-05 15:26:14 -0400

    update

--- app/android/Challenges/app/src/main/java/challenges/com/challenges/fragments/PerfilCriancaFragment.java
+++ app/android/Challenges/app/src/main/java/challenges/com/challenges/fragments/PerfilCriancaFragment.java
@@ -1,27 +1,24 @@
 package challenges.com.challenges.fragments;
 
-import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.squareup.picasso.Picasso;
 
 import challenges.com.challenges.R;
 import challenges.com.challenges.activities.AdicionarAmigoActivity;
 import challenges.com.challenges.config.ConfiguracaoFirebase;
 import challenges.com.challenges.model.Crianca;
-import challenges.com.challenges.model.Usuario;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
@@ -46,8 +43,7 @@ public class PerfilCriancaFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
         View view = inflater.inflate(R.layout.fragment_perfil_crianca, container, false);
@@ -63,7 +59,25 @@ public class PerfilCriancaFragment extends Fragment {
         autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
         String idUsuarioAtual = autenticacao.getCurrentUser().getUid();
 
-        try {
+        //coloquei um listener para ficar escutando as alteraçoes do banco porque é usuário...
+        ConfiguracaoFirebase.getFirestore().collection("Usuarios").document(idUsuarioAtual).addSnapshotListener(getActivity(), new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(DocumentSnapshot documentSnapshot, FirebaseFirestoreException e) {
+                if (documentSnapshot.exists()){
+                    crianca = documentSnapshot.toObject(Crianca.class);
+                    nomeCrianca.setText(crianca.getNome());
+                    if (crianca.getFoto() != null){
+                        Picasso.get().load(crianca.getFoto()).into(fotoCrianca);
+                    }
+                }
+            }
+        });
+
+
+        /**
+         * ANTIGO CÓDIGO OLHE A DIFERENÇA DE UM E OUTRO...
+         * ***/
+       /* try {
             ConfiguracaoFirebase.getFirestore().collection("Usuarios").document(idUsuarioAtual).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                 @Override
                 public void onSuccess(DocumentSnapshot documentSnapshot) {
@@ -79,7 +93,7 @@ public class PerfilCriancaFragment extends Fragment {
         } catch (NullPointerException e) {
             e.printStackTrace();
             System.out.println("caiu excecao");
-        }
+        }*/
 
         adicionarAmigo.setOnClickListener(new View.OnClickListener() {
             @Override
